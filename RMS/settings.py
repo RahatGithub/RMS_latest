c9b6938b566622883999568ab82f827f365b809d
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2023-01-19 14:11:50 +0600

    First commit after bootstrap

--- RMS/settings.py
+++ RMS/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-# handling static files
+# Handling static files
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
@@ -127,6 +127,15 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticFiles')
+
+
+
+# Handling media files
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
